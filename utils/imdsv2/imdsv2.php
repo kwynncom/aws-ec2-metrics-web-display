9896c4334cf8a4b124ebdb2c30489661ec2c2d29
Kwynn Buess <buessresume@gmail.com>
2024-04-01 01:13:19 -0400

    imdsv2 token class starting to work

--- utils/imdsv2/imdsv2.php
+++ utils/imdsv2/imdsv2.php
@@ -2,34 +2,101 @@
 
 require_once('/opt/kwynn/kwutils.php');
 
-class imdsv2Cl {
-    const urlb = 'http://169.254.169.254/latest/meta-data/'; // meta-data is all I care about for now.
+class imdsv2Cl extends dao_generic_4 {
+
+    const testUntil = '2024-04-01 02:00';
+    const dropUntil = '2024-04-01 00:59';
+
+    const urlb = 'http://169.254.169.254/latest'; 
     const timeoutS = 21600;
     const timeoutTestS = 120;
-
+    const timeoutMarginS = 20;
+    const deleteAtS = 0;
+    const minTLen = 20; // actual is roughly 56, including padding-type non-space chars
+    const maxTLen = 150;
+    const dbname = 'imdsv2';
+    const collnm = 'tokens';
+   
+    
     public readonly string $url;
     public readonly bool   $istest;
-    
-    private function __construct(string $url, bool $istest) {
+    private readonly string $vtoken;
+    private readonly int $now;
+    private readonly object $tcoll;
+        
+
+    private function do20() {
+	$t = kwifs($this, 'vtoken');
+	if (!$t) return;
+
+	$c = $this->tcoll;
+	$this->do30();
+    }
+
+
+    private function dbi10() {
+	parent::__construct(self::dbname);
+	$c = $this->kwsel(self::collnm);
+	if ($this->now < strtotime(self::dropUntil)) $c->drop();
+	$c->deleteMany(['expires_at' => ['$lte' => $this->now + self::deleteAtS]]);
+	$this->tcoll = $c;
+    }
+
+    private function do30() {
+	$d = [];
+	$d['token'] = $this->vtoken;
+	$d['expires_at'] = $this->now + $this->getTO() - self::timeoutMarginS;
+	$d['expires_at_r'] = date('r', $d['expires_at']);
+	$this->tcoll->insertOne($d);
+    }
+
+    private function __construct(string $url, bool $istest = false) {
 
 	$this->url = $url;
 	$this->istest = $istest;
+	$this->now = time();
+	$this->dbi10();
 	$this->do10();
+	$this->do20();
+    }
+
+    private function getTO() : int {
+	$test = false;
+	if ($this->now < strtotime(self::testUntil)) $test = true;
+	if ($this->istest) $test = true;
+
+	return $test ? self::timeoutTestS : self::testoutS;
     }
 
     private function oec(string $s) {
-	if (iscli()) echo($s . "\n");
+	if (!iscli()) return;
+	echo($s . "\n");
+    }
+
+    private function setVT($tin) {
+	$tin = trim($tin);
+	if (!$tin) return;
+	if (!is_string($tin)) return;
+	if (!preg_match('/^\S{' . self::minTLen . ',' . self::maxTLen. '}/', $tin)) return;
+	$this->vtoken = $tin;
+	
     }
 
     private function do10() {
+	// if ($this->haveToken()) return;
+	$url = self::urlb . $this->url;
+	$this->oec($url);
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-	$timeout = $this->istest ? self::timeoutTestS : self::testoutS;
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	$timeout = $this->getTO();
 	$this->oec('timeout = ' . $timeout);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-aws-ec2-metadata-token-ttl-seconds' => $timeout]); 
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['X-aws-ec2-metadata-token-ttl-seconds: ' . $timeout]);
+
 	$token = trim(curl_exec($ch));
 	$this->oec($token);
+	$this->setVT($token);
+	$this->oec($this->vtoken);
 
 	unset($timeout);
 
@@ -37,7 +104,7 @@ class imdsv2Cl {
     }
 
     public static function test() {
-	
+	$o = new self('/api/token', true);
     }
 
 }
